7ba249e4ff8a916db61a8fa0e5194f9ff1eda2ce
datlt2306 <datlt2306@gmail.com>
2026-01-08 13:17:39 +0700

    feat: add product detail endpoint and enhance product comments in app.js

--- src/app.js
+++ src/app.js
@@ -6,13 +6,25 @@ app.use(cors());
 
 // khai báo router
 const products = [
-    { id: 1, name: "Product 1", price: 100 },
-    { id: 2, name: "Product 2", price: 200 },
-    { id: 3, name: "Product 3", price: 300 },
+    { id: 1, name: "Product 1", price: 100 }, // product
+    { id: 2, name: "Product 2", price: 200 }, // product
+    { id: 3, name: "Product 3", price: 300 }, // product
 ];
 
+// trả về danh sách sản phẩm
 app.get("/products", (req, res) => {
     return res.json(products);
+});
+// Trả về chi tiết 1 sản phẩm
+app.get("/products/:id", (req, res) => {
+    // trả về 1 sản phẩm
+    const product = products.find((product) => product.id == req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            message: "Khoong tim thay san pham",
+        });
+    }
+    return res.json(product);
 });
 
 // khởi tạo server cổng 3000
