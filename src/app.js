5bdb222457d134e9057947f1b01e0ca8f207ea32
datlt2306 <datlt2306@gmail.com>
2026-01-08 12:55:26 +0700

    feat: add CORS support and create products endpoint in app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
 const app = express();
 
-app.get("/", () => {
-    console.log("Home page");
+app.use(cors());
+
+// khai báo router
+const products = [
+    { id: 1, name: "Product 1", price: 100 },
+    { id: 2, name: "Product 2", price: 200 },
+    { id: 3, name: "Product 3", price: 300 },
+];
+
+app.get("/products", (req, res) => {
+    return res.json(products);
 });
+
+// khởi tạo server cổng 3000
 app.listen(3000, () => {
     console.log("Server đang chạy cổng 3000");
 });
