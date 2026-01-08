d8de6402975e934d92ff5874780b7556d700019d
datlt2306 <datlt2306@gmail.com>
2026-01-08 15:49:01 +0700

    feat: add delete route for products with error handling for non-existent items

--- src/app.js
+++ src/app.js
@@ -19,7 +19,20 @@ app.get("/products/:id", (req, res) => {
     }
     return res.json(product);
 });
-
+app.delete("/products/:id", (req, res) => {
+    // tìm index sản phẩm
+    const index = products.findIndex((item) => item.id == req.params.id);
+    if (index == -1) {
+        return res.status(404).json({
+            message: "Khoong tim thay san pham",
+        });
+    }
+    // Xóa sản phẩm
+    products.splice(index, 1);
+    return res.json({
+        success: true,
+    });
+});
 // Port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
