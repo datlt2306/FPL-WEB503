f77b22363ff5b07b78834a0115a1e265acb1551c
datlt2306 <datlt2306@gmail.com>
2026-01-10 14:56:39 +0700

    refactor: integrate router for API endpoints and streamline app structure

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,71 @@
+const products = [
+    { id: 1, name: "Product 1", price: 101 }, // item
+    { id: 2, name: "Product 2", price: 200 }, // item
+    { id: 3, name: "Product 3", price: 300 }, // item
+];
+
+export const getAll = (req, res) => {
+    try {
+        return res.json(products);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
+export const getOne = (req, res) => {
+    try {
+        const product = products.find((item) => item.id === req.params.id);
+        if (!product) {
+            return res.status(404).json({
+                message: "Product Not found",
+            });
+        }
+        return res.json(product);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
+
+export const createOne = (req, res) => {
+    try {
+        return res.json(products);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
+
+export const deleteOne = (req, res) => {
+    try {
+        const index = products.findIndex((item) => item.id === req.params.id);
+        if (index == -1) {
+            return res.json({
+                message: "Product Not Found",
+            });
+        }
+
+        products.splice(index, 1);
+
+        return res.json({
+            success: true,
+        });
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
+
+export const updateOne = (req, res) => {
+    try {
+        return res.json(products);
+    } catch (error) {
+        return res.status(400).json({
+            message: error,
+        });
+    }
+};
