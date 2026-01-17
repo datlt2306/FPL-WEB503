4efb2160119e5d88341b54ad0e07b774b06f0778
datlt2306 <datlt2306@gmail.com>
2026-01-17 13:27:27 +0700

    feat: Configure request validation to collect all errors instead of aborting early.

--- src/middlewares/validateRequest.js
+++ src/middlewares/validateRequest.js
@@ -2,7 +2,7 @@
 export const validateRequest = (schema, target ='body') => {
     return (req, res, next) => {
          // validate
-        const { error } = schema.validate(req[target]);
+        const { error } = schema.validate(req[target], { abortEarly: false });
         if(error){
             return res.status(400).json({
                 error: "Dữ liệu không hợp lệ",
