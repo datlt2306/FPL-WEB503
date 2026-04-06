75af7b029ceae318f7c0a85913857bb9f1adfce6
datlt2306 <datlt2306@gmail.com>
2026-04-06 10:25:44 +0700

    refactor(app): comment out MongoDB connection and enhance server startup logging

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 
 // Kết nối db
-mongoose.connect(`mongodb://localhost:27017/web503`);
+// mongoose.connect(`mongodb://localhost:27017/web503`);
 // middleware
 
 app.use(express.json());
@@ -14,5 +14,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    const url = `http://localhost:${port}`;
+    console.log(`\x1b[34m\x1b[1mServer is running on port ${port}\x1b[0m`);
+    console.log(`\x1b[36m\x1b[4m\x1b]8;;${url}\x1b\\${url}\x1b]8;;\x1b\\\x1b[0m`);
 });
