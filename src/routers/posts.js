fd4d2f09766c0d5eba384eed4cb4540e128f8d9e
datlt2306 <datlt2306@gmail.com>
2026-04-09 11:08:11 +0700

    feat(posts): enhance CRUD operations with error handling and implement post deletion

--- src/routers/posts.js
+++ src/routers/posts.js
@@ -3,10 +3,9 @@ import { Router } from 'express';
 const postsRouter = Router();
 
 let posts = [
-    { id: 1, title: "Bài viết 1", content: "Nội dung bài viết 1" },
-    { id: 2, title: "Bài viết 2", content: "Nội dung bài viết 2" },
+    { id: 1, title: "Bài viết 1", content: "Nội dung bài viết 1" }, // 0
+    { id: 2, title: "Bài viết 2", content: "Nội dung bài viết 2" }, // 1
 ];
-
 postsRouter.get('/', (req, res) => {
     try {
         return res.json({
@@ -21,7 +20,6 @@ postsRouter.get('/', (req, res) => {
 });
 postsRouter.get('/:id', (req, res) => {
     try {
-        console.log(typeof req.params.id)
         const post = posts.find(post => post.id === parseInt(req.params.id));
         if (!post) return res.status(404).json({
             message: "Bài viết không tồn tại"
@@ -35,19 +33,59 @@ postsRouter.get('/:id', (req, res) => {
 
 });
 postsRouter.post('/', (req, res) => {
-    return res.json({
-        message: "Thêm mới bài viết!"
-    })
+    try {
+        const { title, content } = req.body;
+        const newPost = { id: posts.length + 1, title, content };
+        posts.push(newPost);
+        return res.status(201).json(newPost);
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+
 });
 postsRouter.put('/:id', (req, res) => {
-    return res.json({
-        message: "Cập nhật bài viết!"
-    })
+    try {
+        const post = posts.find(post => post.id === parseInt(req.params.id));
+        if (!post) return res.status(404).json({
+            message: "Bài viết không tồn tại"
+        })
+        const { title, content } = req.body;
+
+
+        console.log(title, content);
+        console.log(post);
+
+        post.title = title || post.title;
+        post.content = content || post.content;
+
+        return res.status(200).json(post);
+
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
 });
 postsRouter.delete('/:id', (req, res) => {
-    return res.json({
-        message: "Xóa bài viết!"
-    })
+    try {
+
+        const index = posts.findIndex(post => post.id === parseInt(req.params.id));
+        console.log(index);
+        if (index === -1) return res.status(404).json({
+            message: "Bài viết không tồn tại"
+        })
+
+        posts.splice(index, 1);
+        return res.json({
+            success: true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
 });
 
 export default postsRouter;
